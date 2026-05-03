70ba707d38cad8f64814c956b1f349ae1c4d5efa
EndDragon438 <quantumhelix@logandigital.com>
2026-05-02 17:07:55 -0700

    endwuzhere

---
+++ tools/endwuzhere.lua
@@ -0,0 +1,56 @@
+-- hi there :3
+
+local color = require("colors")
+local monitor = peripheral.find("monitor")
+local w, h = monitor.getSize()
+
+local text = "end wuz here "
+text = text * (math.floor(w / #text))
+
+local function strMult(str, mult)
+    local temp = str
+    while mult > 1 do
+        temp = temp .. str
+        mult = mult - 1
+    end
+    return temp
+end
+
+local function main()
+    print("hehehehehe")
+    print("hold Ctrl+T to terminate")
+
+    monitor.setBackgroundColor(colors.red)
+    monitor.setTextColor(colors.blue)
+    
+    local str = strMult(text, math.floor(w / #text))
+
+    local hue = 0
+    local posCounter = 0
+    while true do
+        local bgCol = color.hsv_to_rgb(hue, 1, 1)
+        bgCol.r = bgCol.r / 255
+        bgCol.g = bgCol.g / 255
+        bgCol.r = bgCol.b / 255
+        
+        local txtCol = color.hsv_to_rgb(hue + 180 % 360, 1, 1)
+        txtCol.r = txtCol.r / 255
+        txtCol.g = txtCol.g / 255
+        txtCol.r = txtCol.b / 255
+        
+        monitor.setPaletteColor(colors.red, colors.packRGB(bgCol.r, bgCol.g, bgCol.b))
+        monitor.setPaletteColor(colors.blue, colors.packRGB(txtCol.r, txtCol.g, txtCol.b))
+        
+        if posCounter == 0 then
+            monitor.clear()
+        end
+        
+        monitor.setCursorPos(posCounter % w, math.floor(posCounter / w))
+        monitor.write(string.sub(text, posCounter % #text - 1, posCounter % text))
+        
+        hue = hue + 1 % 360
+        posCounter = posCounter + 1 % (w * h)
+    end
+end
+
+main()
