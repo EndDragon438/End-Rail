1dae8a367e3d53f617f288a001e3f435e8427c1d
EndDragon438 <quantumhelix@logandigital.com>
2026-09-07 16:41:32 -0700

    bugfix

--- tools/load.lua
+++ tools/load.lua
@@ -5,15 +5,17 @@ local ram = peripheral.wrap("back")
 local place = peripheral.wrap("bottom")
 -- For playing loaded/not loaded sound
 local speaker = peripheral.wrap("right")
+-- Toggle reversed rotation; either 1 or -1
+local REVERSE = 1
 
 while true do
     -- Wait for redstone signal to fire
     os.pullEvent("redstone")
-    if rs.getInput("back") then
+    if rs.getInput("left") then
         -- Fire the cannon
         rs.setOutput("right", true)
         sleep(0.1)
-        rs.setOuput("right", false)
+        rs.setOutput("right", false)
         
         -- Signal 'not loaded'
         speaker.playNote("bit", 3)
@@ -24,49 +26,49 @@ while true do
         turtle.dig()
         
         -- Load cannon
-        place.rotate(45, -1)
+        place.rotate(45, -1 * REVERSE)
         while place.isRunning() do
             sleep(0.05)
         end
-        place.rotate(45, 1)
+        place.rotate(45, 1 * REVERSE)
         
-        ram.move(2, 1)
+        ram.move(2, 1 * REVERSE)
         while ram.isRunning() do
             sleep(0.05)
         end
-        ram.move(2, -1)
+        ram.move(2, -1 * REVERSE)
         
         while ram.isRunning() do
             sleep(0.05)
         end
         
-        place.rotate(45, -1)
+        place.rotate(45, -1 * REVERSE)
         while place.isRunning() do
             sleep(0.05)
         end
-        place.rotate(45, 1)
+        place.rotate(45, 1 * REVERSE)
         
-        ram.move(3, 1)
+        ram.move(3, 1 * REVERSE)
         while ram.isRunning() do
             sleep(0.05)
         end
-        ram.move(3, -1)
+        ram.move(3, -1 * REVERSE)
         
         while ram.isRunning() do
             sleep(0.05)
         end
         
-        place.rotate(45, -1)
+        place.rotate(45, -1 * REVERSE)
         while place.isRunning() do
             sleep(0.05)
         end
-        place.rotate(45, 1)
+        place.rotate(45, 1 * REVERSE)
         
-        ram.move(4, 1)
+        ram.move(4, 1 * REVERSE)
         while ram.isRunning() do
             sleep(0.05)
         end
-        ram.move(4, -1)
+        ram.move(4, -1 * REVERSE)
         
         -- Replace the breech
         turtle.place()
