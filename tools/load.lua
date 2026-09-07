c44acc111cba4931e13a0e0bc2bc364d65eaec18
EndDragon438 <quantumhelix@logandigital.com>
2026-09-07 13:45:57 -0700

    more edits

--- tools/load.lua
+++ tools/load.lua
@@ -10,7 +10,6 @@ while true do
     -- Wait for redstone signal to load
     os.pullEvent("redstone")
     -- Signal 'not loaded'
-    redstone.setOutput("front", false)
     speaker.playNote("bit", 3)
     
     -- Load cannon
@@ -20,11 +19,11 @@ while true do
     end
     place.rotate(45, 1)
     
-    ram.move(5, 1)
-    while place.isRunning() do
+    ram.move(3, 1)
+    while ram.isRunning() do
         sleep(0.05)
     end
-    ram.move(5, -1)
+    ram.move(3, -1)
     
     sleep(0.55)
     
@@ -34,13 +33,26 @@ while true do
     end
     place.rotate(45, 1)
     
-    ram.move(5, 1)
+    ram.move(3, 1)
+    while ram.isRunning() do
+        sleep(0.05)
+    end
+    ram.move(3, -1)
+    
+    sleep(0.55)
+    
+    place.rotate(45, -1)
     while place.isRunning() do
         sleep(0.05)
     end
-    ram.move(5, -1)
+    place.rotate(45, 1)
+    
+    ram.move(4, 1)
+    while ram.isRunning() do
+        sleep(0.05)
+    end
+    ram.move(4, -1)
     
     -- Signal 'loaded'
-    redstone.setOutput("front", true)
     speaker.playNote("bell", 3)
 end
